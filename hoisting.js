79824fcc2dd3949eed0c6d4c8603d85c9415ab43
saifur703 <saifur703@gmail.com>
2020-04-21 01:08:56 +0600

    understanding hoisting

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+// use function before declare variable
+function myFirst() {
+  return 'My Name is ' + name;
+}
+const name = 'Saifur Rahman';
+console.log(myFirst());
+
+const age = 23;
+function myAge() {
+  return `My age is ${age}`;
+}
+console.log(myAge());
