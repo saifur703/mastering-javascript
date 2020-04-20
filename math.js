9bdb9e9938856ab8c03b08087203bb3a9a854529
saifur703 <saifur703@gmail.com>
2020-04-21 00:03:27 +0600

    Math function

---
+++ math.js
@@ -0,0 +1,18 @@
+// Value of PI
+console.log(Math.PI);
+
+// up
+const n1 = 5.34;
+console.log(Math.ceil(n1));
+
+// down
+const n2 = 5.54;
+console.log(Math.floor(n2));
+
+// dynamic up down
+const n3 = 3.42;
+console.log(Math.round(n3));
+
+// random
+const n4 = 6;
+console.log('Random Number: ', Math.round(Math.random() * n4));
