d4ec95588d913f0e1e3593cb01cf0227c6aa38bd
saifur703 <saifur703@gmail.com>
2020-04-21 00:40:30 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,18 @@
+// for loop
+for (i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// while loop
+let i2 = 0;
+while (i2 <= 10) {
+  console.log(i2, 'Hello');
+  i2++;
+}
+
+// do while loop
+let i3 = 0;
+do {
+  console.log(i3, 'Hello do while loop');
+  i3++;
+} while (i3 <= 10);
